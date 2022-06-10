83c932e35fcea0a984a03151c029319ab278cd76
ryuu <ryuu.public@gmail.com>
2022-06-10 23:54:43 +0900

    fix

---
+++ src/main/java/com/example/jshop/UserController.java
@@ -0,0 +1,45 @@
+package com.example.jshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    /**
+     * example : API for user management
+     */
+
+    // read all users: GET /users
+    @GetMapping
+    public String findAll() {
+        return "FOUND";
+    }
+
+    // create: POST /users
+    @PostMapping
+    public String add() {
+        return "ADDED";
+    }
+
+    // read a user: GET /users/{userId}
+    @GetMapping("/{userId}")
+    public String findById(@PathVariable String userId) {
+        return userId + " FOUND";
+    }
+
+    // update: PATCH /users/{userId}
+    @PatchMapping("/{userId}")
+    public String updatebyId(@PathVariable String userId) {
+        return userId + " UPDATED";
+
+    }
+
+    // delete: DELETE /users/{userId}
+    @DeleteMapping("/{userId}")
+    public String deleteById(@PathVariable String userId) {
+        return userId + " DELETED";
+    }
+
+}
