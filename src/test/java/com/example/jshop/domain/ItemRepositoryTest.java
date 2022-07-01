595644dbb06fa84a3ebaf7acdb1bba7ce396ad6f
ryuu <ryuu.public@gmail.com>
2022-07-01 11:29:51 +0900

    PASSED itemRepositoryTest

---
+++ src/test/java/com/example/jshop/domain/ItemRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.example.jshop.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+
+        //when
+        Item SavedItem = itemRepository.save(item);
+
+        //then
+        Item foundItem = itemRepository.findById(item.getId());
+        Assertions.assertThat(foundItem).isEqualTo(SavedItem);
+    }
+
+    @Test
+    void findById() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+        Item savedItem = itemRepository.save(item);
+
+        //when
+        Item foundItem = itemRepository.findById(item.getId());
+
+        //then
+        Assertions.assertThat(foundItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Item item1 = new Item("itemA", 10000, 10);
+        Item item2 = new Item("itemB", 20000, 20);
+        Item SavedItem1 = itemRepository.save(item1);
+        Item SavedItem2 = itemRepository.save(item2);
+
+        //when
+        List<Item> items = itemRepository.findAll();
+
+        //then
+        Assertions.assertThat(items.size()).isEqualTo(2);
+        Assertions.assertThat(items).contains(item1, item2);
+
+    }
+
+    @Test
+    void update() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+        Item savedItem = itemRepository.save(item);
+        Item newItem = new Item("itemNew", 77777, 77);
+
+        //when
+        itemRepository.update(item.getId(), newItem);
+
+        //then
+        Item foundItem = itemRepository.findById(savedItem.getId());
+        Assertions.assertThat(foundItem.getItemName()).isEqualTo(newItem.getItemName());
+        Assertions.assertThat(foundItem.getPrice()).isEqualTo(newItem.getPrice());
+        Assertions.assertThat(foundItem.getQuantity()).isEqualTo(newItem.getQuantity());
+
+    }
+}
