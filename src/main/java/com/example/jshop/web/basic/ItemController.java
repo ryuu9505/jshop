469c9a7ec7d239c7f0e955b6df7dc00cc492d87a
ryuu <ryuu.public@gmail.com>
2022-07-02 19:56:04 +0900

    FIX

--- src/main/java/com/example/jshop/web/basic/ItemController.java
+++ src/main/java/com/example/jshop/web/basic/ItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -37,9 +38,24 @@ public class ItemController {
     }
 
     @PostMapping("/add")
-    public String addForm(@ModelAttribute Item item, Model model){
-        itemRepository.save(item);
-        return "item";
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String editItem(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/items/{itemId}";
     }
 
     /**
