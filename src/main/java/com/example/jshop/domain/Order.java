676c8bfed1d0abe0d3d429a0a6529ae3e0177b10
ryuu <ryuu.public@gmail.com>
2022-07-09 23:56:19 +0900

    mapping

--- src/main/java/com/example/jshop/domain/Order.java
+++ src/main/java/com/example/jshop/domain/Order.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -20,4 +22,6 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @OneToMany(mappedBy = "order") // mapped by the field
+    private List<OrderItem> orderItems = new ArrayList<>();
 }
---
+++ src/main/java/com/example/jshop/domain/OrderItem.java
@@ -0,0 +1,20 @@
+package com.example.jshop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id") // join the Column
+    private Order order;
+}
