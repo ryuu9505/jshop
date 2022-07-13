6ab26991d9f4e4b7b271c82277b605900a1cdd87
ryuu <ryuu.public@gmail.com>
2022-07-13 11:53:27 +0900

    ADD field LocalDateTime

--- src/main/java/com/example/jshop/domain/Order.java
+++ src/main/java/com/example/jshop/domain/Order.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class Order {
     @GeneratedValue
     @Column(name = "order_id")
     private Long id;
+    private LocalDateTime orderDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
